4599b7b596e5ac56f732dfbfdb29c4800e3cdf86
Bart Volker <b.volker@kirkmancompany.com>
2021-08-29 21:40:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,137 +1,92 @@
-import os
-import plotly.express as px
-import pandas as pd
+import datetime
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from datetime import datetime
-import numpy as np
-from PIL import Image
-import plotly.graph_objects as go
+import plotly
+from dash.dependencies import Input, Output
+
+# pip install pyorbital
+from pyorbital.orbital import Orbital
+satellite = Orbital('TERRA')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-
-#mypath = "G:\Mijn Drive\projecten\wintersport_projectje/"
-
-#img=Image.open(mypath+'achtergrond.png')
-
-def create_text(vertrekdatum):
-#vertrekdatum needs to be in YYYY-MM-DD format
-    my_date = datetime.strptime(vertrekdatum, "%Y-%m-%d")
-    today = datetime.now()
-    delta= my_date-today
-    a= "JAAAAAAAA, Let's GO"
-    b= 'Bijna, nog '+ str(delta.days)+ ' dagen :)'
-    c= 'Nee, nog '+ str(delta.days)+ ' dagen :('
-    if delta.days < 10:
-        return (b)
-    elif delta.days <1:
-        return (a)
-    else:
-        return (c)
-
-
-text = create_text('2022-03-22')
-
-# Create figure
-fig = go.Figure()
-# Constants
-img_width = 1600
-img_height = 900
-scale_factor = 0.5
-# Add invisible scatter trace.
-# This trace is added to help the autoresize logic work.
-fig.add_trace(
-    go.Scatter(
-        x=[0, img_width * scale_factor],
-        y=[0, img_height * scale_factor],
-        mode="markers",
-        marker_opacity=0
-    )
-)
-# Configure axes
-fig.update_xaxes(
-    visible=False,
-    range=[0, img_width * scale_factor]
-)
-
-fig.update_yaxes(
-    visible=False,
-    range=[0, img_height * scale_factor],
-    # the scaleanchor attribute ensures that the aspect ratio stays constant
-    scaleanchor="x"
-)
-# Add image
-"""fig.add_layout_image(
-    dict(
-        x=0,
-        sizex=img_width * scale_factor,
-        y=img_height * scale_factor,
-        sizey=img_height * scale_factor,
-        xref="x",
-        yref="y",
-        opacity=1.0,
-        layer="below",
-        sizing="stretch",
-        source=img)"
-)"""
-fig.add_annotation(x=img_width * scale_factor/2, y=img_height * scale_factor/1.2,
-            text="Gaan we nou al op wintersport?",
-            showarrow=False,
-            yshift=10,
-            font_size=40,
-            font_color='black')
-
-fig.add_annotation(x=img_width * scale_factor/2, y=img_height * scale_factor/1.5,
-            text=text,
-            showarrow=False,
-            yshift=10,
-            font_size=30,
-            font_color='black')
-# Configure other layout
-fig.update_layout(
-    width=img_width * scale_factor,
-    height=img_height * scale_factor,
-    margin={"l": 0, "r": 0, "t": 0, "b": 0},
-)
-
-# Disable the autosize on double click because it adds unwanted margins around the image
-# More detail: https://plotly.com/python/configuration-options/
-fig.show(config={'doubleClick': 'reset'})
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-"""df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")"""
-
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div(
+    html.Div([
+        html.H4('TERRA Satellite Live Feed'),
+        html.Div(id='live-update-text'),
+        dcc.Graph(id='live-update-graph'),
+        dcc.Interval(
+            id='interval-component',
+            interval=1*1000, # in milliseconds
+            n_intervals=0
+        )
+    ])
+)
 
-server = app.server
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+@app.callback(Output('live-update-text', 'children'),
+              Input('interval-component', 'n_intervals'))
+def update_metrics(n):
+    lon, lat, alt = satellite.get_lonlatalt(datetime.datetime.now())
+    style = {'padding': '5px', 'fontSize': '16px'}
+    return [
+        html.Span('Longitude: {0:.2f}'.format(lon), style=style),
+        html.Span('Latitude: {0:.2f}'.format(lat), style=style),
+        html.Span('Altitude: {0:0.2f}'.format(alt), style=style)
+    ]
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
+# Multiple components can update everytime interval gets fired.
+@app.callback(Output('live-update-graph', 'figure'),
+              Input('interval-component', 'n_intervals'))
+def update_graph_live(n):
+    satellite = Orbital('TERRA')
+    data = {
+        'time': [],
+        'Latitude': [],
+        'Longitude': [],
+        'Altitude': []
+    }
+
+    # Collect some data
+    for i in range(180):
+        time = datetime.datetime.now() - datetime.timedelta(seconds=i*20)
+        lon, lat, alt = satellite.get_lonlatalt(
+            time
+        )
+        data['Longitude'].append(lon)
+        data['Latitude'].append(lat)
+        data['Altitude'].append(alt)
+        data['time'].append(time)
+
+    # Create the graph with subplots
+    fig = plotly.tools.make_subplots(rows=2, cols=1, vertical_spacing=0.2)
+    fig['layout']['margin'] = {
+        'l': 30, 'r': 10, 'b': 30, 't': 10
+    }
+    fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
+
+    fig.append_trace({
+        'x': data['time'],
+        'y': data['Altitude'],
+        'name': 'Altitude',
+        'mode': 'lines+markers',
+        'type': 'scatter'
+    }, 1, 1)
+    fig.append_trace({
+        'x': data['Longitude'],
+        'y': data['Latitude'],
+        'text': data['time'],
+        'name': 'Longitude vs Latitude',
+        'mode': 'lines+markers',
+        'type': 'scatter'
+    }, 2, 1)
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-
-
-
